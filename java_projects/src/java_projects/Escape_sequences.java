d856bf29bd86638cd082e0a25e351bb8b408046f
raje2411 <rraman@hw12194.lan>
2018-07-28 14:05:25 -0400

    Escape squences in java

---
+++ java_projects/src/java_projects/Escape_sequences.java
@@ -0,0 +1,17 @@
+package java_projects;
+
+public class Escape_sequences {
+	public static void main(String args[]) {
+		String tab="\t";   //to print two tabs when this "tab" literal is called in the println
+		
+		System.out.println("This Proram is to test on Java Escapate Sequnences");
+		
+		System.out.println("To print \r\"'\" ");
+		
+		System.out.println("To print \fabc\"'\" ");
+		
+		System.out.println("printing tab between 1 and "+ (tab+tab+tab) +" \\t\"3t\" 9999");
+		
+	}
+	
+}
