e01d252d990dee5eafc45a29d4272c68f27cb908
raje2411 <rraman@hw12194.lan>
2018-07-28 22:26:37 -0400

    Casting examples.java

---
+++ java_projects/src/java_projects/Casting_examples.java
@@ -0,0 +1,15 @@
+package java_projects;
+
+public class Casting_examples {
+
+	public static void main(String args[]) {
+		
+		int x=14,y=5;
+		
+		System.out.println("Without Casting y("+y+")/x("+x+ ")  is: "+ (y/x) );	
+		
+		System.out.println("With Casting the result as double y("+y+")/x("+x+ ")  is: "+ (double) (y/x));
+		
+	}
+
+}
